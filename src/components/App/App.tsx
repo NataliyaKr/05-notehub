781a3de56b9a0b73a0df86e6913f47e552e438bf
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2025-07-22 15:38:14 +0200

    components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import Pagination from "../Pagination/Pagination";
+import SearchBox from "../SearchBox/SearchBox";
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <>
+      <div className={css.app}>
+        <header className={css.toolbar}>
+          <SearchBox />
+          <Pagination />
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
